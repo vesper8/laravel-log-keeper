6415ceba2e1216b5e10447b38e4af73f4ea306d5
andream1977 <andrea.manneschi@gmail.com>
2017-08-26 19:26:36 +0200

    update

--- src/Repos/LocalLogsRepo.php
+++ src/Repos/LocalLogsRepo.php
@@ -47,7 +47,7 @@ class LocalLogsRepo implements LogsRepoInterface
     public function compress($log, $compressedName)
     {
         if (windows_os()){
-            $result = gzCompressFile($compressedName);
+            $result = gzCompressFile($log);
 
             if ($result === false) {
                 throw new Exception("Something went wrong when compressing {$log} under win system");
